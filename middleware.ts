bc600b83caa107b2e5260828761eb67ea52ed0fd
junaid <junaid.sadiq009@gmail.com>
2024-07-21 18:16:59 +0500

    fix a minnor error

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,21 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  console.log("SITE_OFFLINE:", process.env.SITE_OFFLINE); // Debugging line
-  const isOffline = process.env["SITE_OFFLINE"] === "true";
+  const isOffline = process.env.SITE_OFFLINE === 'true';
 
-  if (isOffline && req.nextUrl.pathname !== "/offline") {
-    return NextResponse.redirect(new URL("/offline", req.url));
+  // Add logging for debugging
+  console.log('Middleware running');
+  console.log('SITE_OFFLINE:', process.env.SITE_OFFLINE);
+  console.log('isOffline:', isOffline);
+
+  if (isOffline && req.nextUrl.pathname !== '/offline') {
+    return NextResponse.redirect(new URL('/offline', req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/((?!_next|favicon.ico).*)",
+  matcher: '/((?!_next|favicon.ico).*)',
 };
